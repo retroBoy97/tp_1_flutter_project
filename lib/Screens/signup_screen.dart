a93d2ed01c8e51f83d8ea5b28009458c54232494
retroBoy69 <oussamakraiem10@gmail.com>
2025-11-18 19:29:40 +0100

    Exercice 1 et 2

---
+++ lib/Screens/signup_screen.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_input_decoration.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static const String routeName = "/signup";
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  GlobalKey<FormState> formKey = GlobalKey();
+
+  String username = "";
+  String email = "";
+  String password = "";
+  String birthdate = "";
+  String address = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+        backgroundColor: Colors.blue,
+      ),
+
+      body: Form(
+        key: formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+
+          children: [
+
+            // IMAGE DU HAUT
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Image.asset("assets/insat.png", width: 200),
+              ),
+            ),
+
+            // USERNAME
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: CustomInputDecoration(
+                  "UserName",
+                  "Enter your UserName",
+                  const Icon(Icons.person_outline),
+                ).customInputDecoration(),
+
+                onSaved: (value) => username = value!,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "UserName should not be empty";
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            // EMAIL
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: CustomInputDecoration(
+                  "Email",
+                  "Enter your Email",
+                  const Icon(Icons.email_outlined),
+                ).customInputDecoration(),
+
+                onSaved: (value) => email = value!,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Email is required";
+                  }
+                  if (!value.contains("@")) {
+                    return "Invalid email format";
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            // PASSWORD
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                obscureText: true,
+                decoration: CustomInputDecoration(
+                  "Password",
+                  "Enter your Password",
+                  const Icon(Icons.lock_outline),
+                ).customInputDecoration(),
+
+                onSaved: (value) => password = value!,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Password is required";
+                  }
+                  if (value.length < 8) {
+                    return "Password must be at least 8 characters";
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            // BIRTHDATE
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: CustomInputDecoration(
+                  "Birthdate",
+                  "dd/mm/yyyy",
+                  const Icon(Icons.calendar_month),
+                ).customInputDecoration(),
+
+                onSaved: (value) => birthdate = value!,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Birthdate is required";
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            // ADDRESS
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: CustomInputDecoration(
+                  "Address",
+                  "Enter your Address",
+                  const Icon(Icons.home_outlined),
+                ).customInputDecoration(),
+
+                onSaved: (value) => address = value!,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Address is required";
+                  }
+                  return null;
+                },
+              ),
+            ),
+
+            // SUBMIT BUTTON
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("SignUp"),
+                      content: const Text(
+                          "User added successfully! Check your inbox"),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text("OK"),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: const Text("Submit"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
-import 'Screens/HomeScreen/home_screen.dart';
-import 'Screens/Library/library_screen.dart';
-import 'package:tp_1/models/book.dart';
+import 'screens/signup_screen.dart';
 
 void main() {
-  List<Book> books = [
-    Book('Crime et Châtiment', 30, 'assets/Crime et chatiment.jpg'),
-    Book('L\'Homme qui Voulait Être Heureux', 25, 'assets/L\'homme qui voulait etre heureux.jpg'),
-    Book('Les Misérables', 40, 'assets/Les miserables.jpg'),
-  ];
-
-  runApp(MyApp(books: books));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Book> books;
-
-  const MyApp({super.key, required this.books});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LibraryScreen(books: books),
+      debugShowCheckedModeBanner: false,
+
+      // Page affichée au lancement
+      initialRoute: SignUpScreen.routeName,
+
+      // Déclaration des routes
+      routes: {
+        SignUpScreen.routeName: (context) => SignUpScreen(),
+      },
     );
   }
 }
